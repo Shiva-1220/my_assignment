688fa5bb27ad5e6a1f0007ff4d802b35bda42ec1
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-01 13:13:14 +0530

    Create input_output1.cpp

---
+++ input_output1.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int x,y,z;
+    cin>>x>>y>>z;
+    cout<<x+y+z;
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
+
+
+https://www.hackerrank.com/challenges/cpp-input-and-output/submissions/code/325546652
