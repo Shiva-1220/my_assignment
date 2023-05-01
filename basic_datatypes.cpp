76dd2bc383ac12a77b1477963d02406104ecfbbf
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-01 13:15:18 +0530

    Create basic_datatypes.cpp

---
+++ basic_datatypes.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int i;
+    long j;
+    char k;
+    float l;
+    double m;
+    cin>>i;
+    cout<<i<<endl;
+    cin>>j;
+    cout<<j<<endl;
+    cin>>k;
+    cout<<k<<endl;
+    cin>>l;
+    cout<<fixed<<setprecision(3)<<l<<endl;
+    cin>>m;
+    cout<<fixed<<setprecision(9)<<m<<endl;
+    
+    
+
+
+   
+    
+    
+    
+    // Complete the code.
+    return 0;
+}
+
+https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/submissions/code/325703070
