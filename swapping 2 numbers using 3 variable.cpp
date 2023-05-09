5a12c33b2789198f03bb555433527d33c39d36c5
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-10 01:27:44 +0530

    Create swapping 2 numbers using 3 variable.cpp

---
+++ swapping 2 numbers using 3 variable.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout<<"Enter the numbers a,b";
+    cin>>a>>b;
+    c=a; 
+    a=b;
+    b=c;
+    cout<<"a="<<a<<endl;
+    cout<<"b="<<b;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+}
