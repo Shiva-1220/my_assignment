c61eca4f97d3bb6ffc67ad8ffb29ed6e498707c4
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-10 01:30:30 +0530

    Create checking equal numbers.cpp

---
+++ checking equal numbers.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the numbers a,b";
+    cin>>a>>b;
+    if (a==b){
+        cout<<"Numbers are equal.";
+        
+    }
+    else{
+        cout<<"numbers are not same.";
+    }
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+}
