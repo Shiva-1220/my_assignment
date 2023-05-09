4f5dc54bb320095d592b88f4f9f32327ef2dc83c
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-10 01:36:19 +0530

    Create checking leap year or not.cpp

---
+++ checking leap year or not.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long n;
+    cout<<"Enter the YEAR :";
+    cin>>n;
+    if (n%4==0){
+        cout<<"LEAP YEAR.";
+        
+    }
+    else{
+        cout<<"NOT LEAP YEAR.";
+    }
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+}
