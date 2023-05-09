862262fa0651000de8bf90b20b88073f1d0e14b8
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-10 01:33:23 +0530

    Create even odd check.cpp

---
+++ even odd check.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long n;
+    cout<<"Enter the number n";
+    cin>>n;
+    if (n%2==0){
+        cout<<"Number is EVEN.";
+        
+    }
+    else{
+        cout<<"Number is ODD.";
+    }
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+   
+}
