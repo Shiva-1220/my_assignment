37ebb835fd1891c07476a607b08e417e5dc697dc
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-10 01:18:26 +0530

    Create celcius to fahrenheit conversion.cpp

---
+++ celcius to fahrenheit conversion.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    float c,f;
+    cout<<"Enter temperature in Centigrade c ";
+    cin>>c;
+    f=(c*1.8+32) ; //conversion of celcius to fahrenheit.
+    
+    cout<<"Temperature in Fahrenheit is:"<<f<<endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
