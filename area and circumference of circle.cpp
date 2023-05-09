a70f6e5f73dd51505bdd556c668a392bcaf4d597
Shiva-1220 <119787366+Shiva-1220@users.noreply.github.com>
2023-05-10 01:05:28 +0530

    Create area and circumference of circle.cpp

---
+++ area and circumference of circle.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int r,area,circumference;
+    cout<<"Enter radius of circle c ";
+    cin>>r;
+    area=(3.14*r*r); //area of circle.
+    cout<<"Area of circle is:"<<area<<endl;
+    
+    
+    
+    circumference=(2*3.14*r);
+    cout<<"Circumference of circle is:"<<circumference<<endl;
+    
+    
+    
+    
+    return 0;
+}
